f8bb97b89ddbef41b0946b20dceba610319ee11d
EmilySchwartz2 <schwartzemily867@gmail.com>
2025-12-02 23:37:36 -0500

    1235

--- functions/api/saveresults.js
+++ functions/api/saveresults.js
@@ -1,10 +1,18 @@
 // Main handler function for Cloudflare Pages
 export async function onRequestPost(context) {
     const { request, env } = context;
+    try{
         let data;
 
         data = await request.data;
 
         console.log(data);
+    }
+    catch (error){
+        console.log(error);
+    }
+
+
+        return new Response("something", status: 400)
   
 }
