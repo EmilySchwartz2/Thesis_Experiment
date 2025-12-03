ffe6d5f7722cd935e6276da8c588ce452921bc50
Stefan Todorovic <stefantodorovic@gmail.com>
2025-12-03 09:22:45 -0500

    test

--- functions/api/saveresults.js
+++ functions/api/saveresults.js
@@ -10,7 +10,31 @@ export async function onRequestPost(context) {
         data = await request.text();
         console.log("data");
 
+        // data is a string csv attach it as a webhook to a discord channel
         console.log(data);
+        
+        const discord_webhook_url = "https://discord.com/api/webhooks/1445781836742660208/dN-eosLz1ODlANXXtwZD1Bw64vI5nlUyxG18y5Qnw-8gSrt2lAbhopER6PTooNbvG3tc";
+        await fetch(discord_webhook_url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: `New Results Data:\n\`\`\`csv\n${data}\n\`\`\``,
+            }),
+        });
+        
+        return new Response(JSON.stringify({
+            status: 2000,
+        }), {
+            status: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Content-Type': 'application/json',
+            }
+        });
+
     }
     catch (error){
         context.log("error")
